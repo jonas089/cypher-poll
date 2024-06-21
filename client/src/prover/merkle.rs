322733efcee0b6d083c91c12a5057519d9535aae
jonas pauli <jonaspauli089@gmail.com>
2024-06-21 02:15:21 +0200

    add circuit crate

--- client/src/prover/merkle.rs
+++ client/src/prover/merkle.rs
@@ -1,1 +1,8 @@
-// compute a merkle proof for an identity
+use crypto::identity::Identity;
+use kairos_delta_tree::KairosDeltaTree;
+use crate::storage::TreeRoot;
+
+// compute a merkle root for an identity
+pub fn compute_root(snapshot: &mut KairosDeltaTree, leaf: Identity) -> TreeRoot{
+    snapshot.merkle_proof(leaf)
+}
